ec32e7edc0ca61d66a4f0a43c6034d991ec08edc
Xin J Zheng <xin.j.zheng@pwc.com>
2019-09-08 22:34:00 +1000

    initial project

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bye there, Nice to meet you!
         </a>
       </header>
     </div>
